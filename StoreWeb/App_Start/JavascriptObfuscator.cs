32ff091a96b0aefddd91b8a5f16de53154e75176
louislaus <louislaus@qq.com>
2017-08-02 14:54:24 +0800

    添加项目文件。

---
+++ StoreWeb/App_Start/JavascriptObfuscator.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Optimization;
+
+namespace StoreWeb
+{
+    public class JavascriptObfuscator:IBundleTransform
+    {
+        public void Process(BundleContext context, BundleResponse response)
+        {
+            var p = new ECMAScriptPacker(ECMAScriptPacker.PackerEncoding.Normal, true, false);
+            response.Content = p.Pack(response.Content);
+        }
+    }
+}
---
+++ StoreWeb/App_Start/JuCheapScriptBundle.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Optimization;
+
+namespace StoreWeb
+{
+    public class JuCheapScriptBundle:ScriptBundle
+    {
+        readonly JavascriptObfuscator _jso=new JavascriptObfuscator();
+
+        public JuCheapScriptBundle(string virtualPath):base(virtualPath)
+        {
+            Transforms.Add(_jso);
+        }
+    }
+}
